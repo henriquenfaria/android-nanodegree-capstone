c77d98da416f382cf706b17bb41a19d3684fe634
Henrique Faria <henriquenunesfaria@gmail.com>
2017-05-01 18:15:39 -0300

    refactor: Refactor of starter code
    - Move TripsActivity to another package
    - Remove unused menu item
    - Change theme colors

--- app/src/main/java/com/henriquenfaria/wisetrip/ui/TripsActivity.java
+++ app/src/main/java/com/henriquenfaria/wisetrip/ui/TripsActivity.java
@@ -1,18 +1,21 @@
-package com.henriquenfaria.wisetrip;
+package com.henriquenfaria.wisetrip.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.henriquenfaria.wisetrip.R;
+
+
+/* Main Activity that lists all Trips created by the user */
 public class TripsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -52,27 +55,6 @@ public class TripsActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.trips, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
---
+++ app/src/main/java/com/henriquenfaria/wisetrip/util/Utils.java
@@ -0,0 +1,5 @@
+package com.henriquenfaria.wisetrip.util;
+
+/* Utility class with static helper methods */
+public class Utils {
+}
