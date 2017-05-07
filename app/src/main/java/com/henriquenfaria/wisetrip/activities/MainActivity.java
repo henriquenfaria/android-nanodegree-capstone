147c7b451781c698a477e4eee5f22c409ffbb9e2
Henrique Faria <henriquenunesfaria@gmail.com>
2017-05-07 17:49:26 -0300

    fix: Fix navigation drawer initial selected item

--- app/src/main/java/com/henriquenfaria/wisetrip/activities/MainActivity.java
+++ app/src/main/java/com/henriquenfaria/wisetrip/activities/MainActivity.java
@@ -6,7 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -29,10 +30,9 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private boolean mIsTwoPane;
 
     private FirebaseAuth mFirebaseAuth;
-    private  boolean mIsTwoPane;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,7 +73,10 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        if (findViewById(R.id.trip_detail_container) != null) {
+
+        /*TODO: Gonna use master-detail pattern?
+         Seems this is not useful for users, since they will not be constantly switching trips */
+        if (findViewById(R.id.detail_fragment_container) != null) {
             // The detail container view will be present only in the
             // large-screen layouts (res/values-w900dp).
             // If this view is present, then the
@@ -81,10 +84,8 @@ public class MainActivity extends AppCompatActivity
             mIsTwoPane = true;
         }
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        TripListFragment fragment = new TripListFragment();
-        fragmentManager.beginTransaction().replace(R.id.trip_list_container, fragment).commit();
-
+        displaySelectedScreen(R.id.nav_your_trips);
+        navigationView.setCheckedItem(R.id.nav_your_trips);
     }
 
     @Override
@@ -100,14 +101,18 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        int id = item.getItemId();
+        displaySelectedScreen(item.getItemId());
+        return true;
+    }
 
-        // TODO: Implement navigation drawer items
-        if (id == R.id.nav_your_trips) {
+    private void displaySelectedScreen(int itemId) {
+        Fragment fragment = null;
 
-        } else if (id == R.id.nav_settings) {
-
-        } else if (id == R.id.nav_sign_out) {
+        if (itemId == R.id.nav_your_trips) {
+            fragment = new TripListFragment();
+        } else if (itemId == R.id.nav_settings) {
+            // TODO: Implement Settings navigation
+        } else if (itemId == R.id.nav_sign_out) {
             // Sign out
             AuthUI.getInstance()
                     .signOut(this)
@@ -119,14 +124,19 @@ public class MainActivity extends AppCompatActivity
                                 finish();
                             } else {
                                 Toast.makeText(MainActivity.this, R.string
-                                                .toast_sign_out_error, Toast.LENGTH_SHORT).show();
+                                        .toast_sign_out_error, Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
         }
 
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.main_fragment_container, fragment);
+            ft.commit();
+        }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
