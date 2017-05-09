03a7b842d36e7350729989739b541dd7b1f9d94e
Henrique Faria <henriquenunesfaria@gmail.com>
2017-05-09 20:22:12 -0300

    style: Format code

--- app/src/test/java/com/henriquenfaria/wisetrip/ExampleUnitTest.java
+++ app/src/test/java/com/henriquenfaria/wisetrip/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.henriquenfaria.wisetrip;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
